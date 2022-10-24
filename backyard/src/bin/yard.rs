56a428093a5161105bce73c46dba7b6be2045520
gradiuscypher <gradiuscypher@gmail.com>
2022-10-23 19:14:50 -0700

    modularized the yard code

--- backyard/src/bin/yard.rs
+++ backyard/src/bin/yard.rs
@@ -1,15 +1,6 @@
+use backyard::components::plants::{count_trees, spawn_plants};
 use bevy::prelude::*;
 use bevy::time::FixedTimestep;
-
-#[derive(Component)]
-struct Leaf {
-    energy: f32,
-}
-
-#[derive(Bundle)]
-struct Tree {
-    leaf: Leaf,
-}
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
 struct FixedUpdateStage;
@@ -20,39 +11,10 @@ fn main() {
 
     App::new()
         .add_plugins(MinimalPlugins)
-        .add_startup_system(startup)
+        .add_startup_system(spawn_plants)
         .add_stage(
             FixedUpdateStage,
-            update.with_run_criteria(FixedTimestep::step(1.0)),
+            update.with_run_criteria(FixedTimestep::step(5.0)),
         )
         .run();
-}
-
-fn startup(mut commands: Commands) {
-    println!("Starting up ...");
-    let tree = commands
-        .spawn_bundle(Tree {
-            leaf: Leaf { energy: 50.0 },
-        })
-        .insert_bundle(TransformBundle {
-            local: Transform::from_xyz(1.0, 1.0, 0.0),
-            ..Default::default()
-        });
-    let tree = commands
-        .spawn_bundle(Tree {
-            leaf: Leaf { energy: 33.0 },
-        })
-        .insert_bundle(TransformBundle {
-            local: Transform::from_xyz(2.0, 2.0, 0.0),
-            ..Default::default()
-        });
-}
-
-fn count_trees(query: Query<(&Leaf, &Transform)>) {
-    for (leaf, transform) in query.iter() {
-        println!(
-            "leaf at {} has {} energy",
-            transform.translation, leaf.energy,
-        );
-    }
 }
--- backyard/src/components/plants.rs
+++ backyard/src/components/plants.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use rand::Rng;
 
 #[derive(Component)]
 pub struct Leaf {
@@ -8,4 +9,35 @@ pub struct Leaf {
 #[derive(Bundle)]
 pub struct Tree {
     pub leaf: Leaf,
+}
+
+pub fn spawn_plants(mut commands: Commands) {
+    println!("Starting plant spawn...");
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..50 {
+        let x: f32 = rng.gen_range(0..100) as f32;
+        let y: f32 = rng.gen_range(0..100) as f32;
+        let energy: f32 = rng.gen_range(0..100) as f32;
+
+        commands
+            .spawn_bundle(Tree {
+                leaf: Leaf { energy },
+            })
+            .insert_bundle(TransformBundle {
+                local: Transform::from_xyz(x, y, 0.0),
+                ..Default::default()
+            });
+    }
+    println!("Plant spawn complete!");
+}
+
+pub fn count_trees(query: Query<(&Leaf, &Transform)>) {
+    for (leaf, transform) in query.iter() {
+        println!(
+            "leaf at {} has {} energy",
+            transform.translation, leaf.energy,
+        );
+    }
+    println!("-----------------------\n")
 }
